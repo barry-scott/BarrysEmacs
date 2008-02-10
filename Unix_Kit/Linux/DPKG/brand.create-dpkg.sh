9c0426de488bf2ad177ddfc799808693698e54b0
barry <barry@barrys-emacs.org>
2008-02-10 16:17:13 +0000

    add debian package support

---
+++ Unix_Kit/Linux/DPKG/brand.create-dpkg.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+mkdir -p tree/DEBIAN
+
+cat <<EOF >tree/DEBIAN/control
+Package: bemacs
+Version: %(version)s
+Maintainer: Barry Scott <barry@barrys-emacs.org>
+Description: Barry's Emacs Text editor
+ Barry's Emacs Text editor
+ Home page http://www.barrys-emacs.org
+Architecture: i386
+Depends: libmotif3
+EOF
+
+cat <<EOF >tree/DEBIAN/postinst
+#!/bin/sh
+cd /usr/local/bin
+ln -s ../bemacs/bemacs bemacs
+EOF
+chmod +x tree/DEBIAN/postinst
+
+cat <<EOF >tree/DEBIAN/prerm
+#!/bin/sh
+cd /usr/local/bin
+rm -f bemacs
+EOF
+chmod +x tree/DEBIAN/prerm
+
+dpkg-deb -b tree ./
