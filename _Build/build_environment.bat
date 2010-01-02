aa7fcdbb2d083478bc890ffcdfe2d711e80b1313
barry <barry@barrys-emacs.org>
2010-01-02 12:31:31 +0000

    Remove Ridgebuild _Build folder

--- _Build/build_environment.bat
+++
@@ -1,7 +0,0 @@
-@prompt Info:
-set PYTHON_VERSION=22
-set MSVC_VERSION=60
-
-if "%MSVC_VERSION%" == "60" call "C:\Program Files\Microsoft Visual Studio\VC98\Bin\vcvars32.bat"
-if "%MSVC_VERSION%" == "71" call "C:\Program Files\Microsoft Visual Studio .NET 2003\Vc7\bin\vcvars32.bat"
-PATH c:\Python%PYTHON_VERSION%;%PATH%
--- _Build/rbenv.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/echo Usage: . 
-export RB_WORKINGDIR=/u/barry/Latest/V7.1/Emacs
-export RB_CFG_PLATFORM=$(uname -s)
-export RB_CFG_BUILD_TYPE=Release
-set |grep '^RB_'
