daa48fdc02030f4fda53f7594b925ec063f8e236
barry <barry@barrys-emacs.org>
2010-01-02 12:17:26 +0000

    remove unused code

--- Em_User_DLL/em_user_dll.ml
+++
@@ -1,4 +0,0 @@
-(external-function "ml-ext-3-test" "u:\\latest\\emacsv7\\em_user_dll\\debug\\em_user_dll.dll" "ext_3_test")
-(external-function "ml-ext-4-test" "u:\\latest\\emacsv7\\em_user_dll\\debug\\em_user_dll.dll" "ext_4_test")
-(external-function "ml-ext-5-test" "u:\\latest\\emacsv7\\em_user_dll\\debug\\em_user_dll.dll" "ext_5_test")
-(external-function "ml-ext-6-test" "u:\\latest\\emacsv7\\em_user_dll\\debug\\em_user_dll.dll" "ext_6_test")
