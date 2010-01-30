425610cd6c53877bddef149d6782872e11112154
barry <barry@barrys-emacs.org>
2010-01-30 11:22:49 +0000

    Use msvc90 aka 2008

--- Builder/builder_custom_init.cmd
+++ Builder/builder_custom_init.cmd
@@ -4,20 +4,10 @@
 rem builder_custom_init
 set BUILDER_CFG_PLATFORM=Win32
 set BUILDER_CFG_BUILD_TYPE=Release
-set MSVC_VERSION=60
+set MSVC_VERSION=90
 
-if %MSVC_VERSION% == 71 (
-    call "C:\Program Files\Microsoft Visual Studio .NET 2003\Common7\Tools\vsvars32.bat"
-    @echo %__e%
-    call "C:\Program Files\Microsoft Visual C++ Toolkit 2003\vcvars32.bat"
-    @echo %__e%
-    call "c:\Program Files\Microsoft SDK\SetEnv.Bat" /XP32 /RETAIL
-    @echo %__e%
-)
-if %MSVC_VERSION% == 60 (
-    @call "c:\Program Files\Microsoft Visual Studio\VC98\Bin\VCVARS32.BAT"
-    @echo %__e%
-    @call "c:\Program Files\Microsoft SDK\SetEnv.Bat" /XP32 /RETAIL
+if %MSVC_VERSION% == 90 (
+    call "%VS90COMNTOOLS%vsvars32.bat"
     @echo %__e%
 )
 PATH c:\svn;%PATH%
