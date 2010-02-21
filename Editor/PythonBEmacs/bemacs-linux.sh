b69d575cda4d0c2914c8fca8d8de34debbc4094b
barry <barry@barrys-emacs.org>
2010-02-21 13:10:34 +0000

    add helper for linux and rename the macosx one

---
+++ Editor/PythonBEmacs/bemacs-linux.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+export PYTHONPATH=${BUILDER_TOP_DIR}/Editor/PythonBEmacs:${BUILDER_TOP_DIR}/Editor/obj-pybemacs
+export emacs_library=${BUILDER_TOP_DIR}/Kits/Linux/RPM/ROOT/usr/local/bemacs
+
+if [ "$1" = "--gdb" ]
+then
+    shift 1
+    echo
+    echo "run -u be_main.py " "$@" >.gdbinit
+    if [ -e init.gdb ]
+    then
+        cat init.gdb >.gdbinit
+    fi
+    echo
+    gdb python${PYTHON_VERSION}
+
+else
+    python${PYTHON_VERSION} -u be_main.py "$@"
+fi
