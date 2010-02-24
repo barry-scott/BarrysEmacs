9846fad8d571734212795d785c3ab66da514b960
barry <barry@barrys-emacs.org>
2010-02-24 22:58:19 +0000

    build fix

--- Editor/PythonBEmacs/build-linux.sh
+++ Editor/PythonBEmacs/build-linux.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 BIN_DIR=${1:? Bin dir}
-${PYTHON} make-images.py
+${PYTHON} make_be_images.py
 
 cp be_*.py ${BIN_DIR}
 
-echo "#!${PYTHON}" >${BIN_DIR}/bemacs
+echo "#!$( which ${PYTHON} )" >${BIN_DIR}/bemacs
 cat ${BIN_DIR}/be_main.py >>${BIN_DIR}/bemacs
 rm ${BIN_DIR}/be_main.py
+chmod +x ${BIN_DIR}/bemacs
