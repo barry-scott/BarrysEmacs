ffe1d09209ee371ab68faad5e8fa549030a7c77f
barry <barry@barrys-emacs.org>
2010-03-22 22:15:47 +0000

    Support Fedora and Ubuntu

--- Builder/local-install-linux.sh
+++ Builder/local-install-linux.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
-cd ${BUILDER_TOP_DIR}/Kits/Linux/RPM/ROOT
+case ${BUILDER_CFG_PLATFORM:? run builder_init} in
+Linux-Fedora)
+    cd ${BUILDER_TOP_DIR}/Kits/Linux/RPM/ROOT
+    ;;
+
+Linux-Ubuntu)
+    cd ${BUILDER_TOP_DIR}/Kits/Linux/DPKG/tree
+    ;;
+
+*)
+    echo "Error: Unknown BUILDER_CFG_PLATFORM of ${BUILDER_CFG_PLATFORM}"
+    exit 1
+esac
+
 tar czf /tmp/bemacs-tmp.tar.gz usr
 tar xzf /tmp/bemacs-tmp.tar.gz -C /
