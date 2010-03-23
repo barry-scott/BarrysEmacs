f1febfb9ea8012737b1f65fd564295d72d6aac58
barry <barry@barrys-emacs.org>
2010-03-23 21:04:18 +0000

    update to work with fedora or ubuntu

--- Editor/PythonBEmacs/bemacs-linux.sh
+++ Editor/PythonBEmacs/bemacs-linux.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 export PYTHONPATH=${BUILDER_TOP_DIR}/Editor/PythonBEmacs:${BUILDER_TOP_DIR}/Editor/obj-pybemacs
-export BEMACS_EMACS_LIBRARY=${BUILDER_TOP_DIR}/Kits/Linux/RPM/ROOT/usr/local/bemacs8/lib/bemacs
+case "${BUILDER_CFG_PLATFORM}" in
+Linux-Fedora)
+    export BEMACS_EMACS_LIBRARY=${BUILDER_TOP_DIR}/Kits/Linux/RPM/ROOT/usr/local/bemacs8/lib/bemacs
+    ;;
+Linux-Ubuntu)
+    export BEMACS_EMACS_LIBRARY=${BUILDER_TOP_DIR}/Kits/Linux/DPKG/tree/usr/local/bemacs8/lib/bemacs
+    ;;
+*)
+    exit 1
+    ;;
+esac
 
 if [ "$1" = "--gdb" ]
 then
