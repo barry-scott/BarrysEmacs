799a070cd451b7940ccb42b0cd9802e1d14879e9
barry <barry@barrys-emacs.org>
2010-05-09 15:57:37 +0000

    Use all the cores on Macbook Pro

--- Editor/build-macosx.sh
+++ Editor/build-macosx.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "Info: build-macosx.sh Editor - start"
 ${PYTHON} setup.py macosx Makefile
-make -f Makefile "$@"
+make -j 2 -f Makefile "$@"
 echo "Info: build-macosx.sh Editor - end"
