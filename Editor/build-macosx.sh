428e91b8687721ced317930ee383b9ead2823dda
barry <barry@barrys-emacs.org>
2012-06-24 13:22:02 +0000

    We have 8 cores now

--- Editor/build-macosx.sh
+++ Editor/build-macosx.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "Info: build-macosx.sh Editor - start"
 ${PYTHON} setup.py macosx Makefile
-make -j 2 -f Makefile "$@"
+make -j 8 -f Makefile "$@"
 echo "Info: build-macosx.sh Editor - end"
