a6093e77a4cccad0ccaab435ab126f21ad3d94b3
barry <barry@barrys-emacs.org>
2012-08-10 11:21:58 +0000

    use iostreams to avoid issues with size_t type

--- Editor/Utilities/mll2db/mll2db.cpp
+++ Editor/Utilities/mll2db/mll2db.cpp
@@ -13,6 +13,7 @@
 #include <emstring.h>
 #include <emstrtab.h>
 #include <ndbm.h>
+#include <iostream>
 
 char *buf;
 size_t buf_size;
@@ -20,43 +21,39 @@ int verbose(0);
 
 int main(int argc,char **argv)
 {
-    FILE *in;
-    size_t size;
-
     if( argc > 1 && strcmp( argv[1], "-i" ) == 0 )
     {
         verbose = 1; argc--; argv++;
     }
     if( argc < 3 )
     {
-        printf("Usage: mll-2-db [-i] <mll-file> <database>\n");
-        exit(EXIT_FAILURE);
+        std::cerr << "Usage: mll-2-db [-i] <mll-file> <database>" << std::endl;
+        exit( EXIT_FAILURE );
     }
 
-    in = fopen( argv[1], "r" );
+    FILE *in = fopen( argv[1], "r" );
     if( in == NULL )
     {
         perror( argv[1] );
-        exit(EXIT_FAILURE);
+        exit( EXIT_FAILURE );
     }
     fseek( in, 0l, SEEK_END );
-    size = (size_t)ftell( in );
+    size_t size = (size_t)ftell( in );
     fseek( in, 0l, SEEK_SET );
 
     buf = new char[size];
     if( buf == NULL )
     {
-        printf("Failed to allocate %lu bytes\n", buf_size );
-        exit(EXIT_FAILURE);
+        std::cerr << "Failed to allocate " << size << " bytes" << std::endl;
+        exit( EXIT_FAILURE );
     }
 
     buf_size = fread( buf, sizeof( char ), (size_t)size, in );
     if( buf_size > size )
     {
-        printf( "fread overflowed buf!\n");
-        exit(EXIT_FAILURE);
+        std::cerr <<"fread overflowed buf!" << std::endl;
+        exit( EXIT_FAILURE );
     }
-
 
     database db;
     if( !db.open_db( argv[2], 0) )
@@ -80,22 +77,22 @@ int main(int argc,char **argv)
         && buf[pos-1] == ']' )
             {
             if( section_start != 0 )
-        {
-            if( verbose )
-                printf("Adding %.*s (%d bytes)\n", module_length, &buf[module_start], line_start-section_start );
-            status = db.put_db
-                (
-                EmacsString( EmacsString::copy, u_str(&buf[module_start]), module_length ),
-                u_str(&buf[section_start]), line_start-section_start
-                );
-            if( status < 0 )
             {
-                printf ("Database update failed\n");
-                exit(EXIT_FAILURE);
-            }
+                if( verbose )
+                    printf("Adding %.*s (%d bytes)\n", module_length, &buf[module_start], line_start-section_start );
+                status = db.put_db
+                    (
+                    EmacsString( EmacsString::copy, u_str(&buf[module_start]), module_length ),
+                    u_str(&buf[section_start]), line_start-section_start
+                    );
+                if( status < 0 )
+                {
+                    std::cerr << "Database update failed" << std::endl;
+                    exit( EXIT_FAILURE );
+                }
 
-            num_updates++;
-        }
+                num_updates++;
+            }
 
             module_start = line_start + 1;
             module_length = pos - 1 - module_start;
