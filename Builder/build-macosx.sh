79d856268f8f6cd80a196b8940e94100890adf2a
barry <barry@barrys-emacs.org>
2012-08-10 10:23:18 +0000

    add debian support

--- Builder/build-macosx.sh
+++ Builder/build-macosx.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo "Info: build-macosx.sh Builder - start"
-make -f macosx.mak PYTHON=python2.6 clean build
+make -f macosx.mak PYTHON=$PYTHON clean build
 echo "Info: build-macosx.sh Builder - end"
