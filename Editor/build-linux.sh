d69ac4da7637ca4bdc8ece9e7791a70e569397b3
barry <barry@barrys-emacs.org>
2012-08-10 10:27:45 +0000

    arm using Processor intel uses processor

--- Editor/build-linux.sh
+++ Editor/build-linux.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 ${PYTHON} setup.py linux Makefile
-NUM_PROC=$( grep ^processor /proc/cpuinfo |wc -l )
+NUM_PROC=$( grep -i ^processor /proc/cpuinfo |wc -l )
 make -j ${NUM_PROC} -f Makefile "$@"
