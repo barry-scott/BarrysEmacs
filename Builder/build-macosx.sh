a75053f01911cce0e78e864b018535f698faa2d1
barry <barry@barrys-emacs.org>
2015-12-31 12:24:43 +0000

    After a build lauch service will spin up two copies of bemacs_server. Fix is to rebuild its database.

--- Builder/build-macosx.sh
+++ Builder/build-macosx.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 echo "Info: build-macosx.sh Builder - start"
 make -f macosx.mak PYTHON=$PYTHON clean build 2>&1 | tee build.log
+
+echo "Info: rebuild the OS X launch service database"
+CORE=/System/Library/Frameworks/CoreServices.framework/Versions/Current
+${CORE}/Frameworks/LaunchServices.framework/Versions/Current/Support/lsregister \
+ -kill \
+ -r -domain local -domain system -domain user 
+
 echo "Info: build-macosx.sh Builder - end"
