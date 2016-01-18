3d5521945762082d0b7f366829a5681f4fcca0fa
barry <barry@barrys-emacs.org>
2016-01-18 11:09:14 +0000

    Use expected install name of client

--- Editor/PyQtBEmacs/build-win64.cmd
+++ Editor/PyQtBEmacs/build-win64.cmd
@@ -7,5 +7,5 @@ if not "%1" == "" set DIST=%1
 set PYTHONPATH=..\obj-pybemacs
 %PYTHON% -u make_be_images.py
 %PYTHON% -m win_app_packager build be_main.py   --gui %DIST% --name bemacs_server
-%PYTHON% -m win_app_packager build be_client.py --gui %DIST% --name bemacs_client --merge
+%PYTHON% -m win_app_packager build be_client.py --gui %DIST% --name bemacs --merge
 endlocal
