da6592ddb1d7bb74d2257aeeeaeea700bc87928b
barry <barry@barrys-emacs.org>
2016-04-22 18:29:36 +0000

    Put Qt DLLs on the path

--- Editor/PyQtBEmacs/build-win64.cmd
+++ Editor/PyQtBEmacs/build-win64.cmd
@@ -4,8 +4,9 @@ rem
 if "%1" == "" set DIST=dist
 if not "%1" == "" set DIST=%1
 
+set PATH=%BUILDER_QTDIR%\\msvc2015_64\bin;%PATH%
 set PYTHONPATH=..\obj-pybemacs
 %PYTHON% -u make_be_images.py
-%PYTHON% -m win_app_packager build be_main.py   --gui %DIST% --icon ..\Source\Windows\Resources\win_emacs.ico --name bemacs_server
+%PYTHON% -m win_app_packager build be_main.py   --gui %DIST% --icon ..\Source\Windows\Resources\win_emacs.ico --name bemacs_server --verbose
 %PYTHON% -m win_app_packager build be_client.py --gui %DIST% --icon ..\Source\Windows\Resources\win_emacs.ico --name bemacs --merge
 endlocal
