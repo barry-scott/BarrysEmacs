0d911b8c8958ef4825d2a8a8e84ca52d0dd2ea31
barry <barry@barrys-emacs.org>
2016-04-25 12:04:07 +0000

    must not delete tmp here. It is done by Builder\win64.mak clean step

--- Kits/Windows/build-win64.cmd
+++ Kits/Windows/build-win64.cmd
@@ -1,6 +1,4 @@
 setlocal
-if exist tmp rmdir /s /q tmp
-mkdir tmp
 call %PYTHON% setup_kit_files.py %BUILDER_CFG_PLATFORM% %VC_VER%
 call "c:\Program Files (x86)\Inno Setup 5\ISCC.exe" tmp\bemacs.iss
 call tmp\setup_copy.cmd
