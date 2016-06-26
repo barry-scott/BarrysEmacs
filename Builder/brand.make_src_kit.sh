a70f10323b31d7412678e51b9deca6b23fa271a5
Barry Scott <barry@barrys-emacs.org>
2016-06-26 10:23:52 +0100

    update make src kit to use git commands not svn

--- Builder/brand.make_src_kit.sh
+++ Builder/brand.make_src_kit.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
-KITNAME=bemacs-src-%(version)s
-KITDIR=${TMPDIR:-/tmp}/${KITNAME}
+KITNAME=barrys-emacs-src-%(version)s
 
-rm -rf ${KITDIR}
-
-svn export ${BUILDER_TOP_DIR} ${KITDIR}
-cp ~/bin/builder_init ${KITDIR}/Builder
-svnversion -c ${BUILDER_TOP_DIR} > ${KITDIR}/Builder/svn_version.dat
-
-cd ${KITDIR}
-cd Imports
-tar xzf ${TMPDIR:-/tmp}/pycxx-${PYCXX_VER#*:}.tar.gz 
-
-cd ${KITDIR}
 cd ..
-tar czf ${KITNAME}.tar.gz ${KITNAME}
+git archive --format=tar --prefix=BarrysEmacs-%(version)s/ master >${TMPDIR:-/tmp}/${KITNAME}.tar
+
+ls -l ${TMPDIR:-/tmp}/${KITNAME}.tar
