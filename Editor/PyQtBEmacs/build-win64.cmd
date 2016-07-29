93ea7f35422d2e0a3da8ae0114e0a0565b9d1dc7
Barry Scott <barry@barrys-emacs.org>
2016-07-29 11:00:31 +0100

    remove double \\

--- Editor/PyQtBEmacs/build-win64.cmd
+++ Editor/PyQtBEmacs/build-win64.cmd
@@ -4,7 +4,7 @@ rem
 if "%1" == "" set DIST=dist
 if not "%1" == "" set DIST=%1
 
-set PATH=%BUILDER_QTDIR%\\msvc2015_64\bin;%PATH%
+set PATH=%BUILDER_QTDIR%\msvc2015_64\bin;%PATH%
 set PYTHONPATH=..\obj-pybemacs
 %PYTHON% -u make_be_images.py
 %PYTHON% -m win_app_packager build be_main.py   --gui %DIST% --icon ..\Source\Windows\Resources\win_emacs.ico --name bemacs_server --verbose
