95b7ef0faac3e3a368abff274b11c9c670f0e193
Barry Scott <barry@barrys-emacs.org>
2016-08-28 14:32:53 +0100

    for mac use lldb nore gdb

--- Editor/PyQtBEmacs/bemacs-macosx.sh
+++ Editor/PyQtBEmacs/bemacs-macosx.sh
@@ -4,7 +4,7 @@ export BEMACS_EMACS_LIBRARY=${BUILDER_TOP_DIR}/Kits/MacOSX/pkg/Barry\'s\ Emacs-D
 # BArry's quote handling...
 export BEMACS_FIFO=.bemacs8devel/.emacs_command
 
-if [ "$1" = "--gdb" ]
+if [ "$1" = "--lldb" ]
 then
     shift 1
     echo
@@ -15,7 +15,7 @@ then
     fi
     echo "run -u be_main.py " "$@" >>.gdbinit
     echo
-    gdb python${PYTHON_VERSION}
+    lldb -- python${PYTHON_VERSION} -u be_main.py "$@"
 
 else
     which python${PYTHON_VERSION}
