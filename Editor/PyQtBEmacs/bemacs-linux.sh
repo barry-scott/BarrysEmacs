1c2a376c4d96cd2db5d6ed4b0fdca1856057b79a
Barry Scott <barry@barrys-emacs.org>
2016-09-05 16:36:21 +0100

    clean up bemacs-linux.sh that referenced python2.6

--- Editor/PyQtBEmacs/bemacs-linux.sh
+++ Editor/PyQtBEmacs/bemacs-linux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export PYTHONPATH=${BUILDER_TOP_DIR}/Editor/PyQtBEmacs:${BUILDER_TOP_DIR}/Editor/obj-pybemacs
+export PYTHONPATH=${BUILDER_TOP_DIR}/Editor/PyQtBEmacs:${BUILDER_TOP_DIR}/Editor/exe-pybemacs
 case "${BUILDER_CFG_PLATFORM}" in
 Linux-Fedora)
     export BEMACS_EMACS_LIBRARY=${BUILDER_TOP_DIR}/Kits/Linux/RPM/ROOT/usr/local/bemacs8/lib/bemacs
@@ -30,7 +30,6 @@ then
 
     rm -f .gdbinit
 
-    export PYTHONPATH=$PYTHONPATH:/usr/lib64/python${PYTHON_VERSION}:/usr/lib64/python${PYTHON_VERSION}/plat-linux2:/usr/lib64/python${PYTHON_VERSION}/lib-tk:/usr/lib64/python2.6/lib-old:/usr/lib64/python2.6/lib-dynload:/usr/lib64/python2.6/site-packages:/usr/lib64/python2.6/site-packages/PIL:/usr/lib64/python2.6/site-packages/gst-0.10:/usr/lib64/python2.6/site-packages/gtk-2.0:/usr/lib64/python2.6/site-packages/wx-2.8-gtk2-unicode:/usr/lib/python${PYTHON_VERSION}/site-packages
 
     if [ "$1" = "--gdb" ]
     then
