97186409a33d53f3df3eafc16926be09eb1f5234
Barry Scott <barry@barrys-emacs.org>
2016-10-30 10:29:33 +0000

    allow the build commands to have --enable-debug passed into setup.py

--- Editor/build-linux.sh
+++ Editor/build-linux.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-${PYTHON} setup.py linux Makefile
+if [ "$1" == "--enable-debug" ]
+then
+    shift
+    SETUP_OPTIONS=--enable-debug
+fi
+${PYTHON} setup.py linux Makefile ${SETUP_OPTIONS}
 NUM_PROC=$( grep -i ^processor /proc/cpuinfo |wc -l )
 make -j ${NUM_PROC} -f Makefile "$@" 2>&1 | tee build.log
--- Editor/build-windows.cmd
+++ Editor/build-windows.cmd
@@ -1,4 +1,8 @@
-%PYTHON% setup.py win64 Makefile
+if "%1" = "--enable-debug" (
+    %PYTHON% setup.py win64 Makefile --enable-debug
+) else (
+    %PYTHON% setup.py win64 Makefile
+)
     if errorlevel 1 goto :eof
 
 nmake /nologo -e -f Makefile %*
