2cedd3d4bf0be0c3fefef885cd944df821eff923
Barry Scott <barry@barrys-emacs.org>
2016-10-31 13:47:46 +0000

    fix up build-windows handling of --enable-debug

--- Editor/build-windows.cmd
+++ Editor/build-windows.cmd
@@ -1,8 +1,9 @@
-if "%1" = "--enable-debug" (
+if "%1" == "--enable-debug" (
+    shift
     %PYTHON% setup.py win64 Makefile --enable-debug
 ) else (
     %PYTHON% setup.py win64 Makefile
 )
     if errorlevel 1 goto :eof
 
-nmake /nologo -e -f Makefile %*
+nmake /nologo -e -f Makefile %1 %2 %3 %4 %5
