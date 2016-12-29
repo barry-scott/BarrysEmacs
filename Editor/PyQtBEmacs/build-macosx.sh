2533d99a7a769949312bc7489127eaee11909a8c
barry <barry@Expanse.chelsea.private>
2016-12-29 20:02:34 +0000

    remove unused PyQt5 qml files

--- Editor/PyQtBEmacs/build-macosx.sh
+++ Editor/PyQtBEmacs/build-macosx.sh
@@ -27,6 +27,7 @@ pushd "${DIST_DIR}/Barry's Emacs-Devel.app/Contents" >/dev/null
 mkdir -p "Resources/emacs_library"
 mkdir -p "Resources/documentation"
 
+# fixup 1. remove all the PyQt5 files from python35.zip where they cannot be used
 ${PYTHON} <<EOF
 import zipfile
 with zipfile.ZipFile('Resources/lib/python35.zip', 'r') as old:
@@ -40,10 +41,26 @@ EOF
 
 mv Resources/lib/python35clean.zip Resources/lib/python35.zip
 
+# fixup 2. copy all the PyQt5 files from site-packages
 # copy all the installed PyQt5 files as py2app does not copy them all
 cp -R \
     "/Library/Frameworks/Python.framework/Versions/${PY_VER}/lib/python${PY_VER}/site-packages/PyQt5" \
     "Resources/lib/python${PY_VER}/lib-dynload"
+
+# fixup 3. only keep the frameworks that we need, saving space
+# Resources/lib/python3.5/lib-dynload/PyQt5 - QtXxx.so
+mkdir \
+    Resources/lib/python3.5/lib-dynload/PyQt5/tmp
+mv \
+    Resources/lib/python3.5/lib-dynload/PyQt5/Qt?*.so \
+    Resources/lib/python3.5/lib-dynload/PyQt5/tmp
+
+# Resources/lib/python3.5/lib-dynload/PyQt5/Qt/lib - QtXxx.framework
+mkdir \
+    Resources/lib/python3.5/lib-dynload/PyQt5/Qt/lib/tmp
+mv \
+    Resources/lib/python3.5/lib-dynload/PyQt5/Qt/lib/Qt*.framework \
+    Resources/lib/python3.5/lib-dynload/PyQt5/Qt/lib/tmp
 
 for LIBNAME in \
     QtCore \
@@ -55,7 +72,22 @@ for LIBNAME in \
     ;
 do
     echo "Info: framework used ${LIBNAME}"
+    mv \
+            Resources/lib/python3.5/lib-dynload/PyQt5/tmp/${LIBNAME}.so \
+            Resources/lib/python3.5/lib-dynload/PyQt5
+    mv \
+        Resources/lib/python3.5/lib-dynload/PyQt5/Qt/lib/tmp/${LIBNAME}.framework \
+        Resources/lib/python3.5/lib-dynload/PyQt5/Qt/lib
 done
+
+# fixup 4. remove the unused frameworks
+rm -rf Resources/lib/python3.5/lib-dynload/PyQt5/tmp
+rm -rf Resources/lib/python3.5/lib-dynload/PyQt5/Qt/lib/tmp
+
+# remove qml stuff
+rm -rf Resources/lib/python3.5/lib-dynload/PyQt5/Qt/qml
+rm -rf Resources/lib/python3.5/lib-dynload/PyQt5/Qt/translations
+rm -rf Resources/lib/python3.5/lib-dynload/PyQt5/Qt/qsci
 
 if [ "$1" != "--package" ]
 then
