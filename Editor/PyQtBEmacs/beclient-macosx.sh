2893ad9993cc61be3a2846d1fc7cdeb9e1d01ef8
Barry Scott <barry@barrys-emacs.org>
2017-04-28 11:02:19 +0100

    Update to current folder names

--- Editor/PyQtBEmacs/beclient-macosx.sh
+++ Editor/PyQtBEmacs/beclient-macosx.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export BEMACS_FIFO=.bemacs8devel/.emacs_command
-python ${BUILDER_TOP_DIR}/Editor/PythonBEmacs/be_client.py "$@"
+#export BEMACS_FIFO=.bemacs8devel/.emacs_command
+python ${BUILDER_TOP_DIR}/Editor/PyQtBEmacs/be_client.py "$@"
--- Editor/PyQtBEmacs/bemacs-macosx.sh
+++ Editor/PyQtBEmacs/bemacs-macosx.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export PYTHONPATH=${BUILDER_TOP_DIR}/Editor/PythonBEmacs:${BUILDER_TOP_DIR}/Editor/exe-pybemacs
+export PYTHONPATH=${BUILDER_TOP_DIR}/Editor/PyQtBEmacs:${BUILDER_TOP_DIR}/Editor/exe-pybemacs
 export BEMACS_EMACS_LIBRARY=${BUILDER_TOP_DIR}/Kits/MacOSX/pkg/Barry\'s\ Emacs-Devel.app/Contents/Resources/emacs_library
 # Barry's quote handling...
 export BEMACS_FIFO=.bemacs8devel/.emacs_command
