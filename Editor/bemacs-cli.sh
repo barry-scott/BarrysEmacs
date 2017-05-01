8a4969592c9daa4665d8a1663f6e661a2e3add23
Barry Scott <barry@barrys-emacs.org>
2017-05-01 16:03:26 +0100

    Almost always want to remove the old log file

--- Editor/bemacs-cli.sh
+++ Editor/bemacs-cli.sh
@@ -4,9 +4,11 @@ export EMACS_DEBUG_FILE="$TMPDIR/bemacs.log"
 export emacs_user="$HOME/bemacs"
 export emacs_library="${BUILDER_TOP_DIR:?builder_init}/Kits/Linux/RPM/ROOT/usr/local/bemacs8/lib/bemacs"
 
+
 case "$1" in
 --debug=*)
     export EMACS_DEBUG="${1#--debug=}"
+    rm -f ${EMACS_DEBUG_FILE}
     shift
     ;;
 *)
