4ded857b657459217d09409a9a0d3bff75edbd17
Barry Scott <barry@barrys-emacs.org>
2017-05-31 14:37:45 +0100

    We no longer care which distro we are no for the local build and install.

--- Builder/local-install-linux.sh
+++ Builder/local-install-linux.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
-case ${BUILDER_CFG_PLATFORM:? run builder_init} in
-Linux-Fedora)
-    cd ${BUILDER_TOP_DIR}/Kits/Linux/RPM/ROOT
-    ;;
-
-Linux-Debian)
-    cd ${BUILDER_TOP_DIR}/Kits/Linux/DPKG/tree
-    ;;
-
-*)
-    echo "Error: Unknown BUILDER_CFG_PLATFORM of ${BUILDER_CFG_PLATFORM}"
-    exit 1
-esac
+cd ${BUILDER_TOP_DIR?run builder_init}/Kits/Linux/ROOT
 
 tar czf ${TMPDIR:-/tmp}/bemacs-tmp.tar.gz usr
 sudo tar xzf ${TMPDIR:-/tmp}/bemacs-tmp.tar.gz -C /
