00ae83ac9df048db17976fe634e02aa55a67a2ea
Barry Scott <barry@barrys-emacs.org>
2019-01-26 10:24:58 +0000

    Add copy-kit scripts to help manage the binary kits

---
+++ Builder/brand.copy-kit.cmd
@@ -0,0 +1,4 @@
+@echo off
+setlocal
+copy ..\Kits\Windows\tmp\bemacs-%(version)s-setup.exe k:\BEmacs\beta
+endlocal
---
+++ Builder/copy-kit.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+case "${BUILDER_CFG_PLATFORM}" in
+MacOSX)
+    DMG=$( find .. -name '*.dmg' )
+    if [ ! -e /Volumes/Downloads ]
+    then
+        echo "Mounting shares..."
+        open -a mount-shares
+    fi
+
+    for ((i=1; i<=30; i++))
+    do
+        if [ -e /Volumes/Downloads/BEmacs/beta ]
+        then
+            cp -fv "$DMG" /Volumes/Downloads/BEmacs/beta
+            exit $?
+        else
+            sleep 1
+        fi
+    done
+    echo "Downloads is missing"
+    ;;
+
+Linux-Fedora)
+    cp -fv ../Kits/Linux/tmp/${KITNAME}*.x86_64.rpm /shares/Downloads/BEmacs/beta
+    ;;
+
+*)
+    echo "Unsupport platform for copy"
+    ;;
+esac
