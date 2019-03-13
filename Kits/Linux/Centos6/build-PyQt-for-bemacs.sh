7af28b372f013c927a789b74aa97469a02ec2519
Barry Scott <barry@barrys-emacs.org>
2019-03-13 11:10:05 +0000

    Improve script and fix sip build that must make the tools as they are used by the pyqt and qsci builds.

--- Kits/Linux/Centos6/build-PyQt-for-bemacs.sh
+++ Kits/Linux/Centos6/build-PyQt-for-bemacs.sh
@@ -7,6 +7,12 @@ qsci_kit=QScintilla_gpl-2.11.1
 
 num_cores=$( lscpu -p | grep -e '^[0-9]' -c )
 
+if [ "$1" = "" ]
+then
+    echo "Usage: $0 [all|sip|pyqt|qsci]"
+    exit 1
+fi
+
 if [ "$1" = "sip" -o "$1" = "all" ]
 then
     # sip
@@ -15,9 +21,12 @@ then
     rm -rf ${sip_kit}
     tar xf ${sip_kit}.tar.gz
     cd ${sip_kit}
-    ${python} configure.py --sip-module PyQt5.sip --no-tools
+    ${python} configure.py --sip-module PyQt5.sip
     make --jobs=${num_cores}
-    echo -n "Enter sudo password if prompted: "; read A
+    if ! sudo -n true 2>/dev/null
+    then
+        echo -e -n "\e[35;1mEnter sudo password when prompted. (press return to continue)\e[m "; read A
+    fi
     sudo make install
 fi
 
@@ -30,7 +39,10 @@ then
     cd ${pyqt_kit}
     ${python} configure.py --qmake=/usr/bin/qmake-qt5 --confirm-license
     make --jobs=${num_cores}
-    echo -n "Enter sudo password if prompted: "; read A
+    if ! sudo -n true 2>/dev/null
+    then
+        echo -e -n "\e[35;1mEnter sudo password when prompted. (press return to continue)\e[m "; read A
+    fi
     sudo make install
 fi
 
@@ -44,11 +56,17 @@ then
     cd Qt4Qt5
     /usr/bin/qmake-qt5 qscintilla.pro
     make --jobs=${num_cores}
-    echo -n "Enter sudo password if prompted: "; read A
+    if ! sudo -n true 2>/dev/null
+    then
+        echo -e -n "\e[35;1mEnter sudo password when prompted. (press return to continue)\e[m "; read A
+    fi
     sudo make install
     cd ../Python
     ${python} configure.py --pyqt=PyQt5 --qmake=/usr/bin/qmake-qt5
     make --jobs=${num_cores}
-    echo -n "Enter sudo password if prompted: "; read A
+    if ! sudo -n true 2>/dev/null
+    then
+        echo -e -n "\e[35;1mEnter sudo password when prompted. (press return to continue)\e[m "; read A
+    fi
     sudo make install
 fi
