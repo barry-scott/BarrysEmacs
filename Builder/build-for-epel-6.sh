83d72d657409920e378ab1850989e38ed9465bce
Barry Scott <barry@barrys-emacs.org>
2019-06-11 18:25:01 +0100

    Allow overriding of command and release.

--- Builder/build-for-epel-6.sh
+++ Builder/build-for-epel-6.sh
@@ -3,6 +3,7 @@ python3 ./package_bemacs.py ${1:-copr-testing} \
     --colour \
     --mock-target=epel-6-x86_64 \
     --no-gui \
+    --release=${2:-auto} \
     --kit-sqlite=/shared/Downloads/Subversion/sqlite-amalgamation-3280000.zip \
     --kit-xml-preferences=/home/barry/Downloads/xml-preferences-1.1.4.tar.gz \
     --kit-pycxx=/shared/Downloads/Subversion/pycxx-7.1.2.tar.gz
--- Builder/build-for-epel-7.sh
+++ Builder/build-for-epel-7.sh
@@ -2,6 +2,7 @@
 python3 ./package_bemacs.py ${1:-copr-testing} \
     --colour \
     --mock-target=epel-7-x86_64 \
+    --release=${2:-auto} \
     --kit-sqlite=/shared/Downloads/Subversion/sqlite-amalgamation-3280000.zip \
     --kit-xml-preferences=/home/barry/Downloads/xml-preferences-1.1.4.tar.gz \
     --kit-pycxx=/shared/Downloads/Subversion/pycxx-7.1.2.tar.gz
