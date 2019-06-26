aab7991818c1c6f2c1f686e20db21a41f6d0a2b9
Barry Scott <barry@barrys-emacs.org>
2019-06-26 10:53:12 +0100

    Need to be smart about file ownership and permissions. Ensure that files are owner by root:root. When extracting do not overwrite diretory ownership or permissions.

--- Builder/local-install-unix.sh
+++ Builder/local-install-unix.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd ${BUILDER_TOP_DIR?run builder_init}/Builder/tmp/ROOT
 
-tar czf ${TMPDIR:-/tmp}/bemacs-tmp.tar.gz usr
-sudo tar xzf ${TMPDIR:-/tmp}/bemacs-tmp.tar.gz -C /
+# create tar ball as if files are owner by root:root
+tar czf ${TMPDIR:-/tmp}/bemacs-tmp.tar.gz --owner=root --group=root usr
+# do not change meta data on existing directories
+sudo tar xzf ${TMPDIR:-/tmp}/bemacs-tmp.tar.gz --keep-directory-symlink --no-overwrite-dir -C /
