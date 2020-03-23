ce6e633bcdc0c2ab98bad802dad01b397cc92467
Barry Scott <barry@barrys-emacs.org>
2020-03-23 10:39:38 +0000

    Allow building for more then the installed fedora version

--- Builder/build-for-fedora.sh
+++ Builder/build-for-fedora.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-python3 ./package_bemacs.py ${1:-copr-testing} \
-    --colour \
-    --system-ucd
+cmd=${1:-copr-testing}
+rel=${2}
+if [ "${rel}" = "" ]
+then
+    python3 ./package_bemacs.py ${cmd} \
+        --colour \
+        --system-ucd
+
+else
+    python3 ./package_bemacs.py ${cmd} \
+        --mock-target=fedora-${rel}-$(arch) \
+        --colour \
+        --system-ucd
+fi
