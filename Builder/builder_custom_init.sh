b4712c250c85d9cad4412185ef15f75ad9b8b090
Barry Scott <barry@barrys-emacs.org>
2020-08-23 09:49:37 +0100

    defauilt to 3.8

--- Builder/builder_custom_init.sh
+++ Builder/builder_custom_init.sh
@@ -1,7 +1,7 @@
 #!/bin/echo Usage: . $0
 
 export BUILDER_CFG_PLATFORM=$(uname -s)
-export PYTHON_VERSION=${1:-3.7}
+export PYTHON_VERSION=${1:-3.8}
 
 # ether set to tag:<ver> or trunk:<ver>
 export PYCXX_VER=tag7.1.2
