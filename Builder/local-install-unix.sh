b4135a2d7e351fce38c1824eaf9e415db910f9a9
Adam Thompson <arthompson1990@gmail.com>
2020-09-03 22:53:55 +0100

    Fix NetBSD support in local-install-unix.sh
    Also note that, due to the lack of bash-specifics in this script it can
    safely use /bin/sh which makes it more portable.

--- Builder/local-install-unix.sh
+++ Builder/local-install-unix.sh
@@ -1,7 +1,17 @@
-#!/bin/bash
+#!/bin/sh
 cd ${BUILDER_TOP_DIR?run builder_init}/Builder/tmp/ROOT
 
+case "$BUILDER_CFG_PLATFORM" in
+NetBSD)
+    tar_cmd=gtar
+    ;;
+*)
+    tar_cmd=tar
+    ;;
+esac
+
 # create tar ball as if files are owner by root:root
-tar czf ${TMPDIR:-/tmp}/bemacs-tmp.tar.gz --owner=root --group=root usr
+$tar_cmd czf ${TMPDIR:-/tmp}/bemacs-tmp.tar.gz --owner=root --group=root usr
 # do not change meta data on existing directories
-sudo tar xzf ${TMPDIR:-/tmp}/bemacs-tmp.tar.gz --keep-directory-symlink --no-overwrite-dir -C /
+sudo_cmd="$(which sudo)"
+$sudo_cmd $tar_cmd xzf ${TMPDIR:-/tmp}/bemacs-tmp.tar.gz --keep-directory-symlink --no-overwrite-dir -C /
