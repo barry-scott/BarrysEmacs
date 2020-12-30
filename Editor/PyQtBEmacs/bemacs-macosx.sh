f006f9dde70774ec2d2236b5e7958afa0a1b2c0f
Barry Scott <barry@barrys-emacs.org>
2020-12-30 13:12:22 +0000

    Report early that the emacs_library cannot be found.

--- Editor/PyQtBEmacs/bemacs-macosx.sh
+++ Editor/PyQtBEmacs/bemacs-macosx.sh
@@ -3,6 +3,12 @@ export PYTHONPATH=${BUILDER_TOP_DIR}/Editor/PyQtBEmacs:${BUILDER_TOP_DIR}/Editor
 export BEMACS_EMACS_LIBRARY="${BUILDER_TOP_DIR}/Builder/tmp/pkg/Barry's Emacs-Devel.app/Contents/Resources/emacs_library"
 export BEMACS_FIFO=.bemacs8devel/.emacs_command
 
+if [ ! -e "${BEMACS_EMACS_LIBRARY}/emacslib.db" ]
+then
+    echo "Error: emacslib not found: ${BEMACS_EMACS_LIBRARY}/emacslib.db"
+    exit 1
+fi
+
 case "$1" in
 --debug=*)
     export EMACS_DEBUG="${1#--debug=}"
