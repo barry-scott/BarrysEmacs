3f8a54da1659093d62c22da05b4bfba92e662b70
Barry Scott <barry@barrys-emacs.org>
2020-12-30 13:13:00 +0000

    update the style of the source.

--- Editor/Source/Unix/ptyopen_bsd.cpp
+++ Editor/Source/Unix/ptyopen_bsd.cpp
@@ -15,55 +15,48 @@ static const char *pty_second_letters = "0123456789abcdef";
 
 int ptym_open( char *pts_name )
 {
-    int fdm;
-    const char *ptr1;
-    const char *ptr2;
-
-    strcpy(pts_name, "/dev/ptyXY");
-      // array index: 0123456789 (for references in following code)
-    for (ptr1 = pty_first_letters; *ptr1 != 0; ptr1++)
+    strcpy( pts_name, "/dev/ptyXY" );
+    // array index: 0123456789 (for references in following code)
+    for( const char *ptr1 = pty_first_letters; *ptr1 != 0; ptr1++ )
     {
         pts_name[8] = *ptr1;
-        for (ptr2 = pty_second_letters; *ptr2 != 0; ptr2++)
+        for( const char *ptr2 = pty_second_letters; *ptr2 != 0; ptr2++ )
         {
             pts_name[9] = *ptr2;
 
             // try to open master
-            if ( (fdm = open(pts_name, O_RDWR|O_NONBLOCK)) < 0)
+            int fdm = open( pts_name, O_RDWR|O_NONBLOCK );
+            if( fdm < 0 )
             {
-                if (errno == ENOENT)    // different from EIO
-                    return(-1);    // out of pty devices
+                if( errno == ENOENT )   // different from EIO
+                    return -1;          // out of pty devices
                 else
-                    continue;    // try next pty device
+                    continue;           // try next pty device
             }
 
-            pts_name[5] = 't';    // change "pty" to "tty"
-            return(fdm);        // got it, return fd of master
+            pts_name[5] = 't';          // change "pty" to "tty"
+            return fdm;                 // got it, return fd of master
         }
     }
 
-    return(-1);    // out of pty devices
+    return -1;                          // out of pty devices
 }
 
 int ptys_open( int fdm, char *pts_name )
 {
-    struct group    *grptr;
-    int        gid, fds;
-
-    if ( (grptr = getgrnam("tty")) != NULL)
-        gid = grptr->gr_gid;
-    else
-        gid = -1;    // group tty is not in the group file
+    struct group *grptr = getgrnam( "tty" );
+    int gid = grptr != NULL ? grptr->gr_gid : -1;
 
     // following two functions don't work unless we're root
     chown(pts_name, getuid(), gid);
     chmod(pts_name, S_IRUSR | S_IWUSR | S_IWGRP);
 
-    if ( (fds = open(pts_name, O_RDWR|O_NONBLOCK)) < 0)
+    int fds = open(pts_name, O_RDWR|O_NONBLOCK);
+    if( fds < 0 )
     {
-        close(fdm);
-        return(-1);
+        close( fdm );
+        return -1;
     }
 
-    return(fds);
+    return fds;
 }
