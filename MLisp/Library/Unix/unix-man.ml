0e7eaccbdfd3e148c87141103d93a95282a30911
Barry Scott <barry@barrys-emacs.org>
2021-01-08 19:30:43 +0000

    Support colouring man output on OS that use ANSI escape sequences.

--- MLisp/Library/Unix/unix-man.ml
+++ MLisp/Library/Unix/unix-man.ml
@@ -5,7 +5,8 @@
 
         (setq ~prefix prefix-argument-provided)
         (save-excursion
-            (forward-character)
+            (if (! (eobp))
+                (forward-character))
             (backward-word)
             (set-mark)
             (forward-word)
@@ -26,14 +27,55 @@
 (defun
     man (~word (get-tty-string ": man "))
     ~overstrike-length
+    ~text
+
     (pop-to-buffer "man")
     (erase-buffer)
     (set-mark)
     (message "man " ~word " fetching text...")(sit-for 0)
-    (filter-region (concat "man " ~word))
+    (filter-region (concat "MAN_KEEP_FORMATTING=1 man " ~word))
     (unset-mark)
     (message "man " ~word " cleaning up text...")(sit-for 0)
     (beginning-of-file)
+
+    ; Either there are ANSI escapes of back-space overstrikes
+
+
+    ;
+    ; replace ansi escape
+    ; 
+    (beginning-of-file)
+    (error-occurred
+        (replace-string "\e[4m\e[22m" "\e[22m\e[4m"))
+    (error-occurred
+        (ere-replace-string "( +)\e\\[22m" "\e[22m\\1"))
+    (error-occurred
+        (while
+            1
+            (ere-search-forward "\e\\[1m([^\e]*)\e\\[(0|22)m")
+            (region-around-match 1)
+            (setq ~text (region-to-string))
+            (region-around-match 0)
+            (erase-region)
+            (insert-string ~text)
+            (apply-colour-to-region (dot) (- (dot) (length ~text)) 6)
+        )
+    )
+    (beginning-of-file)
+    (error-occurred
+        (while
+            1
+            (ere-search-forward "\e\\[4m([^\e]*)\e\\[(0|24)m")
+            (region-around-match 1)
+            (setq ~text (region-to-string))
+            (region-around-match 0)
+            (erase-region)
+            (insert-string ~text)
+            (apply-colour-to-region (dot) (- (dot) (length ~text)) 3)
+        )
+    )
+    (unset-mark)
+
     ;
     ; calibrate the overstrike amount
     ;
