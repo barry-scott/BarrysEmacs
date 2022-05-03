0e3416ddc6001adc8bf6096a29f04698c563ff32
Barry Scott <barry@barrys-emacs.org>
2022-05-03 19:24:49 +0100

    Add RPM-specfile-add-changelog-entry Fix case of rpm-specfile-mode.key to match the .ml

--- MLisp/Library/rpm-specfile-mode.ml
+++ MLisp/Library/rpm-specfile-mode.ml
@@ -1,6 +1,11 @@
 ;
 ; RPM-specfile mode
 ;
+(declare-global
+    RPM-specfile-authors-email)
+(if (= (type-of-expression RPM-specfile-authors-email) "integer")
+    (setq RPM-specfile-authors-email ""))
+
 (defun
     (RPM-specfile-mode
         (setq mode-string "RPM-specfile")
@@ -10,6 +15,53 @@
         ; defaults for grep
         (error-occurred (setq grep-default-files "*.spec"))
         (novalue)
+    )
+)
+
+(defun
+    (RPM-specfile-add-changelog-entry
+        ~current-date
+        ~version
+        ~release
+
+        (setq ~current-date (current-time))
+        ; change from: Sun Feb 13 12:17:23 2022
+        ; to Sun Feb 13 2022
+        (setq ~current-date
+            (concat
+                (substr ~current-date 0 11)
+                (substr ~current-date -4 4)
+            )
+        )
+
+        (if (= RPM-specfile-authors-email "")
+            (error-message "RPM-specfile-authors-email has not been set"))
+
+        (beginning-of-file)
+        (save-excursion
+            (ere-search-forward "^Version:\\s*([.0-9]+)")
+            (region-around-match 1)
+            (setq ~version (region-to-string))
+        )
+        (save-excursion
+            (ere-search-forward "^Release:\\s*([0-9]+)")
+            (region-around-match 1)
+            (setq ~release (region-to-string))
+        )
+
+        (ere-search-forward "^%changelog")
+        (next-line)
+        (beginning-of-line)
+        ; * <date> <email> - <version>-<release>
+        ; - message
+        ;   message more
+        (insert-string
+            "* " ~current-date " " RPM-specfile-authors-email
+            " - " ~version "-" ~release "\n"
+            "- \n"
+            "\n"
+        )
+        (backward-character 2)
     )
 )
 
@@ -114,7 +166,7 @@
     (define-keymap "RPM-specfile-ESC-map")
     (use-local-map "RPM-specfile-map")
     (local-bind-to-key "RPM-specfile-ESC-map" "\e")
-    (error-occurred (execute-mlisp-file "RPM-specfile-mode.key"))
+    (error-occurred (execute-mlisp-file "rpm-specfile-mode.key"))
 
     (use-syntax-table "RPM-specfile")
 
