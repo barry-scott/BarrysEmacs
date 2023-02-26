eb50f740773c49bf868b3db584b6860213b80117
Barry Scott <barry@barrys-emacs.org>
2023-02-26 09:21:40 +0000

    update list of versions

--- Builder/build-for-all.sh
+++ Builder/build-for-all.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 cmd=${1?cmd}
-./build-for-epel-6.sh "${cmd}"
 ./build-for-epel-7.sh "${cmd}"
 ./build-for-epel-8.sh "${cmd}"
-./build-for-fedora.sh "${cmd}" 31
-./build-for-fedora.sh "${cmd}" 32
-./build-for-fedora.sh "${cmd}" 33
+./build-for-fedora.sh "${cmd}" 36
+./build-for-fedora.sh "${cmd}" 37
